984fd756df9378330049261c782c171cb9ad69a3
David Salami <wizdave97@gmail.com>
2020-06-19 17:09:48 +0100

    feat: enabled flutterwave payment integration

--- public/javascripts/payment.js
+++ public/javascripts/payment.js
@@ -1,73 +1,68 @@
-var checkoutForm = document.getElementById('checkoutForm');
 
-checkoutForm.addEventListener('submit', payWithPaystack, false);
+const pay = document.getElementById('checkoutForm');
 
-function payWithPaystack(event) {
-  event.preventDefault();
-  const email = document.getElementById('email').value.trim();
-  const amount = document.getElementById('totalPrice').textContent.trim();
-  const phone = document.getElementById('phone').value.trim();
-  const address = document.getElementById('address').value.trim();
-  const sender_psid = document.getElementById('sender_psid').textContent.trim();
-  const name = document.getElementById('fullname').value.split(' ').filter(str => (Boolean(str)));
-  const config = {
 
-    key: 'pk_test_e413e8381c8b5bf384fd2afee1dc43b8b965c087', // Replace with your public key
+pay.addEventListener('submit', makePayment);
 
-    email: email,
+function makePayment(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value.trim();
+    const amount = document.getElementById('totalPrice').textContent.trim();
+    const phone = document.getElementById('phone').value.trim();
+    const address = document.getElementById('address').value.trim();
+    const sender_psid = document.getElementById('sender_psid').textContent.trim();
+    const name = document.getElementById('fullname').value;
+    if (!email || !amount || !phone || !sender_psid || !name || !address) {
+        alert('Please fill the form correctly');
+        return;
+    }
+    FlutterwaveCheckout({
+      public_key: "FLWPUBK-0c4fd187aa15166455427e8fe8f156ba-X",
+      tx_ref: `${Date.now()}-${email}`,
+      amount: +amount,
+      currency: "NGN",
+      payment_options: "card",
+      customer: {
+        email: email,
+        phone_number: phone,
+        name: name,
+      },
+      callback: function (data) {
+        if (data.status === 'successful') {
+           
+            const body = {
+                sender_psid: +sender_psid,
+                order_number: Date.now(),
+                fullname: name.join(' '),
+                phone: phone,
+                address: address,
+                paid: true,
+                transaction_ref: data.tx_ref
+                }
+                alert('Payment Successful! Reference: ' + reference + '\nClose this alert and wait while your order is completed');
+                fetch('https://sheltered-headland-16417.herokuapp.com/checkout',{
+                    method:'POST',
+                    body: JSON.stringify(body)
+                    }
+                ).then(resp => resp.json()).then(resp => {
+                    alert(resp.msg);
+                }).catch(err => {
+                    alert("Could not process transaction, please contact our customer service with your reference number for a refund");
+                })
+                // Make an AJAX call to your server with the reference to verify the transaction
 
-    amount: +amount * 100, // the amount value is multiplied by 100 to convert to the lowest currency unit
-
-    currency: 'NGN', // Use GHS for Ghana Cedis or USD for US Dollars
-
-    firstname: name[0],
-
-    lastname: name[name.length - 1],
-
-    reference: `${Date.now()}-${email}`, // Replace with a reference you generated
-
-    callback: function(response) {
-
-      //this happens after the payment is completed successfully
-      
-      const reference = response.reference;
-      const body = {
-          sender_psid: +sender_psid,
-          order_number: Date.now(),
-          fullname: name.join(' '),
-          phone: phone,
-          address: address,
-          paid: true,
-          transaction_ref: reference
-      }
-      alert('Payment Successful! Reference: ' + reference + '\nClose this alert and wait while your order is completed');
-      fetch('https://sheltered-headland-16417.herokuapp.com/checkout',{
-          method:'POST',
-          body: JSON.stringify(body)
         }
-      ).then(resp => resp.json()).then(resp => {
-          alert(resp.msg);
-      }).catch(err => {
-          alert("Could not process transaction, please contact our customer service with your reference number for a refund");
-      })
-      // Make an AJAX call to your server with the reference to verify the transaction
-
-    },
-
-    onClose: function() {
-
-      alert('Transaction was not completed please try again, window closed.');
-
-    },
-  }  
-  if (email && amount && phone && sender_psid && name && address)
-  {
-    const paystackPopup = new PaystackPop(config);
-    paystackPopup.open();
-  }
-  else{
-      alert('Please fill the form correctly')
+        else {
+            alert("Payment was not successful");
+        }
+      },
+      customizations: {
+        title: "Apple Products Store",
+        description: "Payment for items in cart",
+        logo: "https://assets.piedpiper.com/logo.png",
+      },
+    });
   }
   
-
-}
+  
+  
